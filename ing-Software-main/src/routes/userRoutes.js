79147d7162d562da05f02daa0bade92b40333133
Damrod14 <99446798+Damrod14@users.noreply.github.com>
2024-06-28 01:18:38 -0600

    Delete ing-Software-main directory

--- ing-Software-main/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createUser } from "../controllers/userController.js";
-
-const userRoutes = express.Router();
-
-userRoutes.post("/", createUser);
-
-export { userRoutes };
