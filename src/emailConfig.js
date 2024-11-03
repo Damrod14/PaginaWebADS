81637c151986eab110fcfb4ff8c08190ab8019d3
Damrod14 <erickrodriguez932@gmail.com>
2024-11-03 17:59:34 -0600

    cambiosDeCorreo

--- src/emailConfig.js
+++ src/emailConfig.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'erodriguezm1406@gmail.com', 
+    user: 'erickrodriguez932@gmail.com', 
     pass: 'qzjo awde tabv udkv'   
   }
 });
